aac8c0c6a4c994c488c49ac429c24b5abee489b1
shenjinghao <shenjinghao@chunyu.me>
2017-05-31 20:11:52 +0800

    HEAD First 设计模式读书笔记及代码

---
+++ JHDesignPattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JHDesignPattern
+//
+//  Created by Shenjinghao on 2017/5/31.
+//  Copyright © 2017年 SJH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JHDesignPattern/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  JHDesignPattern
+//
+//  Created by Shenjinghao on 2017/5/31.
+//  Copyright © 2017年 SJH. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
