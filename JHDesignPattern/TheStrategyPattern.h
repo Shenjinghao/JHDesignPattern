5f08efe38ea9a8478ee5430d1fe5011a1c72cd34
shenjinghao <shenjinghao@chunyu.me>
2017-06-01 17:11:02 +0800

    HEAD First 2-策略模式

---
+++ JHDesignPattern/TheStrategyPattern.h
@@ -0,0 +1,16 @@
+//
+//  TheStrategyPattern.h
+//  JHDesignPattern
+//
+//  Created by Shenjinghao on 2017/6/1.
+//  Copyright © 2017年 SJH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol TheStrategyPattern <NSObject>
+
+//策略模式：定义算法族，分别封装起来，让他们之间可以互相替换，
+//功能：让算法的变化独立于使用算法的人
+
+@end
