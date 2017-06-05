9775a8ef76c3306e6d9c1dae666a3d09302be7a7
shenjinghao <shenjinghao@chunyu.me>
2017-06-05 19:17:52 +0800

    collectionview

---
+++ JHDesignPattern/JHCollectionViewCell.h
@@ -0,0 +1,17 @@
+//
+//  JHCollectionViewCell.h
+//  JHDesignPattern
+//
+//  Created by Shenjinghao on 2017/6/5.
+//  Copyright © 2017年 SJH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JHCollectionViewCell : UICollectionViewCell
+
+@property (nonatomic, strong) UILabel *titleLabel;
+
+@property (nonatomic, strong) UILabel *countLabel;
+
+@end
